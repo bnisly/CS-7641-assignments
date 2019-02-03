d864d2698de03ca779ace5525e1f79a80fedb28e
bnisly <Barry_Nisly@intuit.com>
2019-02-03 11:00:34 -0800

    Ensure max_iter is capped for SVM

--- assignment1/experiments/SVM.py
+++ assignment1/experiments/SVM.py
@@ -6,6 +6,7 @@ import sklearn
 import experiments
 import learners
 
+MAX_ITER=2000
 
 class SVMExperiment(experiments.BaseExperiment):
     def __init__(self, details, verbose=False):
@@ -20,7 +21,7 @@ class SVMExperiment(experiments.BaseExperiment):
         gamma_fracs = np.arange(1/features, 2.1, 0.2)
         tols = np.arange(1e-8, 1e-1, 0.01)
         C_values = np.arange(0.001, 2.5, 0.25)
-        iters = [-1, int((1e6/samples)/.8)+1]
+        iters = [MAX_ITER, int((1e6/samples)/.8)+1]
 
         best_params_linear = None
         best_params_rbf = None
