28a407aa0f332bbf87219d5dcee3f12d2349943d
bnisly <Barry_Nisly@intuit.com>
2019-02-03 22:10:05 -0800

    Add seed and comment out tar, cp, and shutdown commands

--- assignment1/doit.sh
+++ assignment1/doit.sh
@@ -15,8 +15,11 @@ date
 #for i in dt boosting knn ann svm; do
 for i in svm ann; do
     aws sns publish --region us-west-2 --phone-number $1 --message "starting $i"
-    nice time python run_experiment.py --threads -2 --$i --verbose
+    time nice python run_experiment.py --threads -2 --$i --verbose --seed 12345
     aws sns publish --region us-west-2 --phone-number $1 --message "$i done at $(date)"
 done
 date
 
+#tar zcf ~/dt-output-72core.tgz output*
+#aws s3 cp ~/dt-output-72core.tgz s3://bnisly-results/dt-output-72core.tgz
+#sudo shutdown now
